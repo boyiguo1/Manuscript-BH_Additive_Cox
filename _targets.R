df3a0e352ab754c472debbd9809885828aae7a58
Boyi Guo <boyiguo1@uab.edu>
2021-10-19 17:13:09 -0500

    Create Manuscript Structure

--- _targets.R
+++ _targets.R
@@ -18,4 +18,36 @@ tar_plan(
 
 # tar_target(target2, function_to_make2(arg)) ## targets style
 
+
+
+# Manuscript --------------------------------------------------------------
+
+#* Section Paths ####
+
+tar_target(bib_path,
+           "Manuscript/bibfile.bib",
+           format = "file"),
+
+tar_target(intro_path,
+           "Manuscript/01-intro.Rmd",
+           format = "file"),
+
+tar_target(method_path,
+           "Manuscript/02-method.Rmd",
+           format = "file"),
+
+tar_target(sim_path,
+           "Manuscript/03-simulation.Rmd",
+           format = "file"),
+
+tar_target(real_data_path,
+           "Manuscript/04-real_data.Rmd",
+           format = "file"),
+
+tar_target(disc_path,
+           "Manuscript/05-conclusion.Rmd",
+           format = "file"),
+
+tar_render(manu, "Manuscript/00-main.Rmd",
+           output_file = "CPH_AM.pdf")
 )
