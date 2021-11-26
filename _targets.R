1d788dffc73655439c66f40a0bfbc8eb8cb59005
Boyi Guo <boyiguo1@uab.edu>
2021-11-26 15:25:12 -0600

    Add simulation summary

--- _targets.R
+++ _targets.R
@@ -5,7 +5,8 @@ options(tidyverse.quiet = TRUE)
 
 tar_option_set(
   packages = c("tidyverse", "knitr", "rmarkdown",
-               "simsurv")
+               "simsurv",
+               "unglue")
 )
 
 ## Load your packages, e.g. library(targets).
@@ -17,18 +18,34 @@ lapply(list.files("./R", full.names = TRUE, recursive = TRUE), source)
 ## tar_plan supports drake-style targets and also tar_target()
 tar_plan(
 
-  # Manuscript --------------------------------------------------------------
-  #* Section Paths ####
-  tar_files(manu_path,
-            setdiff(
-              grep("*.Rmd|*.bib",
-                   list.files("Manuscript", full.names = TRUE),
-                   value = TRUE
-              ),
-              'Manuscript/00-main.Rmd'
-            )
+
+  # Simulation --------------------------------------------------------------
+  tar_files(
+    sim_res_path,
+    list.files("/data/user/boyiguo1/bcam/Res",
+               full.names = TRUE)
   ),
 
-  tar_render(manu, "Manuscript/00-main.Rmd",
-             output_file = "CPH_AM.pdf")
+
+  tar_target(
+    sim_summary,
+    generate_simulation_summary(sim_res_path)
+  ),
+
+
+
+# Manuscript --------------------------------------------------------------
+#* Section Paths ####
+tar_files(manu_path,
+          setdiff(
+            grep("*.Rmd|*.bib",
+                 list.files("Manuscript", full.names = TRUE),
+                 value = TRUE
+            ),
+            'Manuscript/00-main.Rmd'
+          )
+),
+
+tar_render(manu, "Manuscript/00-main.Rmd",
+           output_file = "CPH_AM.pdf")
 )
