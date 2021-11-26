6694521cd2a106cb8e440b0c73221cbb86c39959
Boyi Guo <boyiguo1@uab.edu>
2021-11-26 09:26:54 -0600

    Update repo

--- _targets.R
+++ _targets.R
@@ -13,27 +13,9 @@ tar_option_set(
 
 ## Load your R files
 lapply(list.files("./R", full.names = TRUE, recursive = TRUE), source)
-source("./Sim/Code/helper_functions.R")
-# lapply(list.files("./Sim/Code/", full.names = TRUE, recursive = TRUE), source)
 
 ## tar_plan supports drake-style targets and also tar_target()
 tar_plan(
-
-  # target = function_to_make(arg), ## drake style
-
-  # tar_target(target2, function_to_make2(arg)) ## targets style
-
-
-  # Simulation --------------------------------------------------------------
-
-  # tar_target(cns_pi, c(0.15, 0.3, 0.45)),
-  # tar_target(sim_it, 1:10),
-
-  # ** Examine Find Censoring Rate Function ---------------------------------
-  # tar_target(cns_rate_batch, run_cns_rate_test(cns_pi), pattern = cross(cns_pi, map(sim_it))),
-
-
-
 
   # Manuscript --------------------------------------------------------------
   #* Section Paths ####
