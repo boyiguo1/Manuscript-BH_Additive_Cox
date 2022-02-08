948e8be2913eca065c6cd2cf055522c5e677b74d
Boyi Guo <boyiguo1992@gmail.com>
2022-02-08 12:01:04 -0600

    Set up target manuscript flow

--- _targets.R
+++ _targets.R
@@ -6,7 +6,7 @@ options(tidyverse.quiet = TRUE)
 tar_option_set(
   packages = c("tidyverse", "knitr", "rmarkdown",
                "ggplot2", "simsurv",
-               "unglue")
+               "unglue", "xtable")
 )
 
 ## Load your packages, e.g. library(targets).
@@ -20,89 +20,89 @@ tar_plan(
 
 
   # Simulation --------------------------------------------------------------
-  tar_files(
-    sim_res_path,
-    list.files("/data/user/boyiguo1/bcam/Res",
-               full.names = TRUE)
-  ),
-
-
-  tar_target(
-    sim_summary,
-    generate_simulation_summary(sim_res_path)
-  ),
-
-  tar_target(
-    sim_pred_measure_raw,
-    generate_simulation_res_raw(sim_summary)
-  ),
-
-
-  tar_target(
-    sim_cnr_prop,
-    sim_pred_measure_raw %>%
-      rowwise %>%
-      mutate(mean_cns = mean(unlist(sim_pi_cns)), sd_cns = sd(unlist(sim_pi_cns))) %>%
-      ungroup() %>%
-      select(-c(sim_pi_cns, train, test))
-  ),
-
-
-  tar_target(
-    sim_fail_prop,
-    generate_simulation_mdl_failed_rate(sim_pred_measure_raw)
-  ),
-
-  tar_target(
-    sim_train_res,
-    sim_pred_measure_raw %>%
-      generate_sim_pred_measure("train")
-  ),
-
-  tar_target(
-    sim_test_res,
-    sim_pred_measure_raw %>%
-      generate_sim_pred_measure("test")
-  ),
-
-  tar_target(
-    metrics,
-    c("deviance", "Cindex")
-  ),
-
-
-  # Tabulate Simulation Results
-  tar_target(
-    sim_train_tab,
-    tab_sim_res(sim_train_res, sec = metrics),
-    pattern = map(metrics),
-    iteration = "list"
-  ),
-
-  # Visualize Simulation Results
-  tar_target(
-    sim_train_viz,
-    viz_sim_res(sim_train_res, sec = metrics),
-    pattern = map(metrics),
-    iteration = "list"
-  ),
-
-
-  # Tabulate Simulation Results
-  tar_target(
-    sim_test_tab,
-    tab_sim_res(sim_test_res, sec = metrics),
-    pattern = map(metrics),
-    iteration = "list"
-  ),
-
-  # Visualize Simulation Results
-  tar_target(
-    sim_test_viz,
-    viz_sim_res(sim_test_res, sec = metrics),
-    pattern = map(metrics),
-    iteration = "list"
-  ),
+  # tar_files(
+  #   sim_res_path,
+  #   list.files("/data/user/boyiguo1/bcam/Res",
+  #              full.names = TRUE)
+  # ),
+  #
+  #
+  # tar_target(
+  #   sim_summary,
+  #   generate_simulation_summary(sim_res_path)
+  # ),
+  #
+  # tar_target(
+  #   sim_pred_measure_raw,
+  #   generate_simulation_res_raw(sim_summary)
+  # ),
+  #
+  #
+  # tar_target(
+  #   sim_cnr_prop,
+  #   sim_pred_measure_raw %>%
+  #     rowwise %>%
+  #     mutate(mean_cns = mean(unlist(sim_pi_cns)), sd_cns = sd(unlist(sim_pi_cns))) %>%
+  #     ungroup() %>%
+  #     select(-c(sim_pi_cns, train, test))
+  # ),
+  #
+  #
+  # tar_target(
+  #   sim_fail_prop,
+  #   generate_simulation_mdl_failed_rate(sim_pred_measure_raw)
+  # ),
+  #
+  # tar_target(
+  #   sim_train_res,
+  #   sim_pred_measure_raw %>%
+  #     generate_sim_pred_measure("train")
+  # ),
+  #
+  # tar_target(
+  #   sim_test_res,
+  #   sim_pred_measure_raw %>%
+  #     generate_sim_pred_measure("test")
+  # ),
+  #
+  # tar_target(
+  #   metrics,
+  #   c("deviance", "Cindex")
+  # ),
+  #
+  #
+  # # Tabulate Simulation Results
+  # tar_target(
+  #   sim_train_tab,
+  #   tab_sim_res(sim_train_res, sec = metrics),
+  #   pattern = map(metrics),
+  #   iteration = "list"
+  # ),
+  #
+  # # Visualize Simulation Results
+  # tar_target(
+  #   sim_train_viz,
+  #   viz_sim_res(sim_train_res, sec = metrics),
+  #   pattern = map(metrics),
+  #   iteration = "list"
+  # ),
+  #
+  #
+  # # Tabulate Simulation Results
+  # tar_target(
+  #   sim_test_tab,
+  #   tab_sim_res(sim_test_res, sec = metrics),
+  #   pattern = map(metrics),
+  #   iteration = "list"
+  # ),
+  #
+  # # Visualize Simulation Results
+  # tar_target(
+  #   sim_test_viz,
+  #   viz_sim_res(sim_test_res, sec = metrics),
+  #   pattern = map(metrics),
+  #   iteration = "list"
+  # ),
 
 # Manuscript --------------------------------------------------------------
 #* Section Paths ####
