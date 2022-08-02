6c4cee4c17d6fd94873ea7d3ce2d75655723816c
Boyi Guo <boyiguo1992@gmail.com>
2022-08-02 09:55:30 -0500

    Correct X-axis direction

--- R/plot_true_sig.R
+++ R/plot_true_sig.R
@@ -1,8 +1,8 @@
 plot_true_sig <- function(){
 
-  # f_1 <- function(x) (x+1)^2/5
-  # f_2 <- function(x) exp(x+1)/25
-  # f_3 <- function(x) 3*sin(x)/2
+  # f_1 <- function(x) (x*5-2)^2/20-3
+  # f_2 <- function(x) exp(x+1)/10
+  # f_3 <- function(x) 2*sin(x*4)
   # f_4 <- function(x) (1.4*x+0.5)/2
 
   plot_list <- list(f_1, f_2, f_3, f_4) %>%
@@ -10,7 +10,7 @@ plot_true_sig <- function(){
       x <- seq(from = -2, to = 2, by = 0.1)
       y <- sim_fun(x)
       ggplot() +
-        xlim(2, -2) +
+        xlim(-2, 2) +
         ylab("f(x)") +
         geom_function(fun = sim_fun) +
         geom_smooth(aes(x,y), method = "lm",
