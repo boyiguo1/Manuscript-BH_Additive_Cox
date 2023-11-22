e95f86f93af7d42baf870f463829045b01804b70
Boyi Guo <boyi.guo.work@gmail.com>
2023-11-22 10:32:20 -0500

    refactor

--- R/generate_simulation_results_raw.R
+++ R/generate_simulation_results_raw.R
@@ -1,9 +1,8 @@
 generate_simulation_res_raw <- function(sim_summary){
 
   sim_summary %>%
-    pmap_dfr(.f = function(n_success, path, ...){
-
-      if(n_success == 0)        # Failed Simulation
+    pmap_dfr(.f = function(n_success_it, path, ...){
+      if(n_success_it == 0)        # Failed Simulation
         return(data.frame(NULL))
 
       ret <- list.files(path, full.names = TRUE) %>%
@@ -20,10 +19,12 @@ generate_simulation_res_raw <- function(sim_summary){
       # return object
       tibble(
         ...,                   # Copy over simulation parameters
-        n_success,
+        n_success_it,
         sim_pi_cns = ret %>% pull(p.cen) %>% list(),
-        train = ret %>% select(starts_with("train_res")) %>% rename_all(str_sub, start=11)%>% list(),
-        test = ret %>% select(starts_with("test_res")) %>% rename_all(str_sub, start=10)%>% list()
+        train = ret %>% select(starts_with("train_res")) %>%
+          rename_all(str_sub, start=11)%>% list(),
+        test = ret %>% select(starts_with("test_res")) %>%
+          rename_all(str_sub, start=10)%>% list()
       )
     })
 }
